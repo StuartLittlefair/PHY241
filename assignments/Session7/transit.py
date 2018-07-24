1b6e2d2ad90ed58b55fee058abca38c5ae075dee
Stuart Littlefair <s.littlefair@shef.ac.uk>
2018-07-24 14:53:54 +0000

    2018 version

--- assignments/Session7/transit.py
+++ assignments/Session7/transit.py
@@ -1,12 +1,13 @@
 import numpy as np
 from scipy import integrate
 
-def transit(t,Rp,Rs,i,P,t0,mu):
+
+def transit(t, Rp, Rs, i, P, t0, mu):
     """Calculate the transit shape due to an exoplanet.
-    
+
     This function implements the model of exoplanetary transits found in
     Sackett et al (1999, ASIC, 532, 189). 
-    
+
     Args
     ----
     t: np.ndarray, float
@@ -23,7 +24,7 @@ def transit(t,Rp,Rs,i,P,t0,mu):
         Time of mid-transit. Units of days (MJD)
     mu: float
         linear limb-darkening parameter for star
-    
+
     Returns
     --------
     np.ndarray, float
@@ -31,8 +32,9 @@ def transit(t,Rp,Rs,i,P,t0,mu):
     """
     t = np.atleast_1d(t)
     return np.array([_transit(tval,Rp,Rs,i,P,t0,mu) for tval in t])
-    
-def _transit(t,Rp,Rs,i,P,t0,mu):
+
+
+def _transit(t, Rp, Rs, i, P, t0, mu):
     """Calculates transit at single time"""
     t -= t0
     i = np.radians(i)
@@ -41,31 +43,32 @@ def _transit(t,Rp,Rs,i,P,t0,mu):
 
     if d>Rs+Rp:
         return 1.0
-        
+
     def LD(x,mu):
         return 1-mu*(1-np.sqrt(1-(x/Rs)**2))
-        
+
     def integrand(x):
         costheta = (d**2 + x**2 - Rp**2) / 2. / x / d
-        if  costheta < -1:
+        if  costheta < -1 or costheta > 1:
             theta = np.pi
         else:
             theta = np.arccos(costheta)
         return x*LD(x,mu)*theta
-    
+
     # bottom integral in eqn 12 of sackett reduces to
     fac = np.pi*(3-mu)*Rs**2/6.
-    
+
     lolim = max(0,d-Rp)
     uplim = min(Rs,d+Rp)
-    
+
     A, err =  integrate.quad(integrand,lolim,uplim)
     return 1-A/fac
-    
+
+
 if __name__ == "__main__":
     import matplotlib.pyplot as plt
     plt.style.use('seaborn-muted')
-    
+
     from astropy import units as u, constants as const
     # WASP-4b
     a = 0.02312*u.au
@@ -75,9 +78,9 @@ if __name__ == "__main__":
     i = 88.6
     mu=0.311
     t0 = 54748.150490
-    t = np.linspace(t0-4./24.,t0+4./24,1000)
-    y = transit(t,rp,Rs,i,P,t0,mu)
+    t = np.linspace(t0-4./24., t0+4./24, 1000)
+    y = transit(t, rp, Rs, i, P, t0, mu)
     x,yd,e = np.loadtxt('wasp4_transit.txt').T
-    plt.errorbar(x,yd,yerr=e,fmt='.')
-    plt.plot(t,y)
+    plt.errorbar(x, yd, yerr=e, fmt='.')
+    plt.plot(t, y)
     plt.show()
